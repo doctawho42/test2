09a9796bdadd43bf2dac95358376fc05f58b02a8
Nikita Polomoshnov <plomoshnovnikita00@gmail.com>
2019-01-05 20:50:19 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/my-image.png') {
+      myImage.setAttribute ('src','images/image2.png');
+	} else {
+	  myImage.setAttribute ('src','images/my-image.png');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Dragon Inc. - future is coming, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Dragon Inc. - future is coming, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
